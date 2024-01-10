7ec629dcc184b9b9c9fa97793330f8afb739d51a
Pietromuresu <muresupietro66@gmail.com>
2024-01-10 12:50:02 +0100

    Create routes for store locator (example routes)

--- store-locator-front/src/router.jsx
+++ store-locator-front/src/router.jsx
@@ -1,21 +1,45 @@
-import {createBrowserRouter} from 'react-router-dom';
+import {Navigate, createBrowserRouter} from 'react-router-dom';
 import Login from '../src/views/Login';
-import Signup from'../src/views/Signup';
 import Users from'../src/views/Users';
+import Signup from'../src/views/Signup';
 import NotFound from'../src/views/Notfound';
+import GuestLayout from './components/GuestLayout';
+import DefaultLayout from './components/DefaultLayout';
+import Dashboard from './views/Dashboard';
 
 const router = createBrowserRouter([
     {
-        path: '/login',
-        element: <Login />
+        path: '/',
+        element: <DefaultLayout />,
+        children:[
+            {
+                path: '/',
+                // Redirect the user to /users
+                element: <Navigate to="/users" />
+            },
+            {
+                path: '/users',
+                element: <Users />
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            },
+        ]
     },
     {
-        path: '/signup',
-        element: <Signup />
-    },
-    {
-        path: '/users',
-        element: <Users />
+        path: '/',
+        element: <GuestLayout />,
+        children:[
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+        ]
     },
     {
         path: '*',
