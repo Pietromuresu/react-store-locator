ba164ed8b64f1a0a6a011d878eab6299d5972c14
Pietromuresu <muresupietro66@gmail.com>
2024-01-10 16:29:41 +0100

    Create context to keep the session  after refresh

--- store-locator-front/src/components/DefaultLayout.jsx
+++ store-locator-front/src/components/DefaultLayout.jsx
@@ -1,8 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function DefaultLayout(){
+
+    const {user, token} =  useStateContext();
+    // Se l'utente non è loggato fallo loggare
+    if(!token){
+        return <Navigate to={"/login"} />
+    }
     return(
         <div>
+            Default
             <Outlet />
         </div>
     )
--- store-locator-front/src/contexts/ContextProvider.jsx
+++ store-locator-front/src/contexts/ContextProvider.jsx
@@ -1,3 +1,37 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
 
-const StateContext = createContext()
+//
+const StateContext = createContext({
+    currentUser: null,
+    token: null,
+    setUser: ()=>{},
+    setToken: ()=>{}
+})
+
+export const ContextProvider = ({children}) =>{
+
+    const [user, SetUser] = useState({});
+    const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
+
+    const setToken = (token) =>{
+        _setToken(token);
+        if(token) {
+            localStorage.setItem('ACCESS_TOKEN', token);
+        }else{
+            localStorage.removeItem('ACCESS_TOKEN');
+        }
+    }
+
+    return (
+        <StateContext.Provider value={{
+            user,
+            token,
+            SetUser,
+            setToken
+        }}>
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () =>  useContext(StateContext);
