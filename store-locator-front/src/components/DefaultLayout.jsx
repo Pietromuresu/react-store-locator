fe3203439f0a05327ed235f074d85e1fafabbd3a
Pietromuresu <muresupietro66@gmail.com>
2024-01-10 16:39:11 +0100

    Use context provider

--- store-locator-front/src/components/DefaultLayout.jsx
+++ store-locator-front/src/components/DefaultLayout.jsx
@@ -4,10 +4,12 @@ import { useStateContext } from "../contexts/ContextProvider";
 export default function DefaultLayout(){
 
     const {user, token} =  useStateContext();
-    // Se l'utente non è loggato fallo loggare
+    // Se l'utente non è loggato fallo loggare per non arrivare a dati protetti
+
     if(!token){
         return <Navigate to={"/login"} />
     }
+
     return(
         <div>
             Default
